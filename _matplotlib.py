6fc2c71395f138b044b4c5f981a83e45cd1c4f1a
gustavo9601 <tavo9601@gmail.com>
2020-07-20 16:13:19 -0500

    Matplotlib basico

---
+++ _matplotlib.py
@@ -0,0 +1,25 @@
+# pip install matplotlib
+
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+# seteamos el estilo
+style.use('ggplot')
+print("estilos disponibles | style.available", style.available)
+
+# proporcionamos 2 listas [x, y]
+plt.plot(
+    [1,2,3,4,5],
+    [23,22,12,32,5]
+)
+plt.title('Resultados')
+plt.xlabel('Semestre')
+plt.ylabel('Notas')
+
+
+
+
+
+
+
+plt.show()
